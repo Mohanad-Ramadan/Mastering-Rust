6602d90f8a045e53250d83a14e9b459b3c49a414
Mohanad-Ramadan <mohanadramadan.m1@gmail.com>
2025-09-22 09:13:01 +0300

    Finish Chapter 2

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,17 +1,36 @@
+use core::num;
 use std::io;
+use std::cmp::Ordering;
+
 use rand::Rng;
 
 fn main() {
+    println!("Guess the number!");
+
     let random_number = rand::thread_rng().gen_range(1..=100);
     
-    println!("the secret number is : {random_number}");
-    println!("please enter your guss");
-
-    let mut guss = String::new();
-
-    io::stdin()
+    loop {
+        println!("please enter your guss");
+        
+        let mut guss = String::new();
+        
+        io::stdin()
         .read_line(&mut guss)
         .expect("Fail to read line");
-
-    println!("You guessed: {guss}");
+        
+        let guss: i32 = match guss.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+        match guss.cmp(&random_number) {
+            Ordering::Equal => {
+                println!("You gussed it right, You win!!");
+                break;
+            }
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Less => println!("Too samll!")
+        }
+    }
+    
 }
